dbac44e07c5c9753f6a73455db984cd8788c1f9a
Ivan Shaposhnyk <dark.merlose.omg@gmail.com>
2026-04-22 19:10:52 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/rechnung_flow/RechnungFlowService/RechnungFlowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rechnung_flow.RechnungFlowService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechnungFlowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechnungFlowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rechnung_flow/RechnungFlowService/RechnungFlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rechnung_flow.RechnungFlowService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechnungFlowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
